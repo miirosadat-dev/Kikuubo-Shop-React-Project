7a4b3bb1750db6b7ebc7d49bec4e678e6b2c3b77
SADAT MIIRO <miirosadat0@gmail.com>
2026-08-02 21:41:27 +0300

    Add initial components and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
 
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
