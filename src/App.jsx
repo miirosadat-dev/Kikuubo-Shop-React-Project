c90aabc611a414cc62fb83c1c0a83134dd72b152
SADAT MIIRO <miirosadat0@gmail.com>
2026-08-05 11:39:22 +0300

    Add shop page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+
+import { setProducts } from "./redux/productSlice";
+import { Products } from "./assets/Products";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setProducts(Products));
+  }, [dispatch]);
+
   return (
     <BrowserRouter>
       <Navbar />
+
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
       </Routes>
+
       <Footer />
     </BrowserRouter>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import banner1 from "../assets/images/banner1.png";
 import InfoSection from "../components/InfoSection";
 import CategorySection from "../components/CategorySection";
-import { setProducts } from "../redux/productSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { Products, Categories } from "../assets/Products";
 import ProductCard from "../components/ProductCard";
+import Shop from "./Shop";
 
 const Home = () => {
-  const dispatch = useDispatch();
   const products = useSelector((state) => state.product.products);
 
   const [selectedCategory, setSelectedCategory] = useState("All");
@@ -17,78 +16,78 @@ const Home = () => {
       ? products
       : products.filter((product) => product.category === selectedCategory);
 
-  useEffect(() => {
-    dispatch(setProducts(Products));
-  }, []);
   return (
-    <div className="bg-white mt-2 px-4 md:px-16 lg:px-24">
-      <div
-        className="container mx-auto py-4
-       flex flex-col md:flex-row space-x-2"
-      >
-        <div className="w-full md:w-3/12">
-          <div
-            className="bg-red-600 text-white
-          text-xs font-bold px-2 py-2.5"
-          >
-            SHOP BY CATEGORIES
-          </div>
-          <ul className="space-y-4 bg-gray-100 p-3 border border-gray-300">
-            {Categories.map((category) => (
-              <li
-                key={category}
-                onClick={() => setSelectedCategory(category)}
-                className="flex items-center cursor-pointer text-sm font-medium"
-              >
-                <div className="w-2 h-2 border border-red-500 rounded-full mr-2"></div>
-                {category}
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        <div className="w-full md:w-9/12 mt-8 md:mt-0 h-100 relative">
-          <img src={banner1} alt="hero image" />
-          <div className="absolute lg:top-15 md:top-10 top-1 lg:left-10 md:left-8 left-3">
-            <p className="text-gray-600 lg:mb-4 mb-1 text-sm">
-              Sadat | Kikuubo Shop
-            </p>
-            <h2 className="lg:text-5xl lg:mb-5 md:mb-2 mb-0 text-sm font-bold">
-              WELCOME TO
-              <br /> KIKUUBO SHOP
-            </h2>
-            <p className="lg:text-2xl lg:mb-5 md:mb-2 mb-1 text-sm lg:mt-1.5 md:mt-1 mt-0 font-bold text-gray-800">
-              MILLIONS+ PRODUCTS
-            </p>
-            <button
-              className="bg-red-600 lg:text-3xl text-sm px-8 lg:py-1 md:py-1 py-0.3 text-white lg:mt-4 mt-0
-            hover:bg-red-700 transform transition-transform duration-300 hover:scale-105"
-            >
-              SHOP NOW
-            </button>
-          </div>
-        </div>
-      </div>
-
-      <InfoSection />
-      <CategorySection />
-
-      <div className="container mx-auto py-12">
-        <h2
-          className="text-2xl font-bold
-        mb-6 text-center"
-        >
-          Top Products
-        </h2>
+    <div>
+      <div className="bg-white mt-2 px-4 md:px-16 lg:px-24">
         <div
-          className="grid grid-cols-1 sm:grid-cols-1
-        md:grid-cols-3 lg:grid-cols-5 gap-6 cursor-pointer"
+          className="container mx-auto py-4
+       flex flex-col md:flex-row space-x-2"
         >
-          {filteredProducts.slice(0, 5).map((product) => (
-            <ProductCard product={product} key={product.id} />
-          ))}
+          <div className="w-full md:w-3/12">
+            <div
+              className="bg-red-600 text-white
+          text-xs font-bold px-2 py-2.5"
+            >
+              SHOP BY CATEGORIES
+            </div>
+            <ul className="space-y-4 bg-gray-100 p-3 border border-gray-300">
+              {Categories.map((category) => (
+                <li
+                  key={category}
+                  onClick={() => setSelectedCategory(category)}
+                  className="flex items-center cursor-pointer text-sm font-medium"
+                >
+                  <div className="w-2 h-2 border border-red-500 rounded-full mr-2"></div>
+                  {category}
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="w-full md:w-9/12 mt-8 md:mt-0 h-100 relative">
+            <img src={banner1} alt="hero image" />
+            <div className="absolute lg:top-15 md:top-10 top-1 lg:left-10 md:left-8 left-3">
+              <p className="text-gray-600 lg:mb-4 mb-1 text-sm">
+                Sadat | Kikuubo Shop
+              </p>
+              <h2 className="lg:text-5xl lg:mb-5 md:mb-2 mb-0 text-sm font-bold">
+                WELCOME TO
+                <br /> KIKUUBO SHOP
+              </h2>
+              <p className="lg:text-2xl lg:mb-5 md:mb-2 mb-1 text-sm lg:mt-1.5 md:mt-1 mt-0 font-bold text-gray-800">
+                MILLIONS+ PRODUCTS
+              </p>
+              <button
+                className="bg-red-600 lg:text-3xl text-sm px-8 lg:py-1 md:py-1 py-0.3 text-white lg:mt-4 mt-0
+            hover:bg-red-700 transform transition-transform duration-300 hover:scale-105"
+              >
+                SHOP NOW
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <InfoSection />
+        <CategorySection />
+
+        <div className="container mx-auto py-12">
+          <h2
+            className="text-2xl font-bold
+        mb-6 text-center"
+          >
+            Top Products
+          </h2>
+          <div
+            className="grid grid-cols-1 sm:grid-cols-1
+        md:grid-cols-3 lg:grid-cols-5 gap-6 cursor-pointer"
+          >
+            {filteredProducts.slice(0, 5).map((product) => (
+              <ProductCard product={product} key={product.id} />
+            ))}
+          </div>
         </div>
       </div>
+      <Shop />
     </div>
   );
 };
