9e04918599a68598b37c9a285e576a9cc2c866bb
SADAT MIIRO <miirosadat0@gmail.com>
2026-08-05 10:59:50 +0300

    Add top products

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import CategorySection from "../components/CategorySection";
 import { setProducts } from "../redux/productSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { Products, Categories } from "../assets/Products";
+import ProductCard from "../components/ProductCard";
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -72,11 +73,19 @@ const Home = () => {
       <InfoSection />
       <CategorySection />
 
-      <div>
-        <h2>Top Products</h2>
-        <div>
+      <div className="container mx-auto py-12">
+        <h2
+          className="text-2xl font-bold
+        mb-6 text-center"
+        >
+          Top Products
+        </h2>
+        <div
+          className="grid grid-cols-1 sm:grid-cols-1
+        md:grid-cols-3 lg:grid-cols-5 gap-6 cursor-pointer"
+        >
           {filteredProducts.slice(0, 5).map((product) => (
-            <div key={product.id}>{product.name}</div>
+            <ProductCard product={product} key={product.id} />
           ))}
         </div>
       </div>
