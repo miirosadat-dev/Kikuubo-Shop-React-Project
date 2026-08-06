6693b9594ee7f023c791f4b8b64c45c3f01cd7c1
SADAT MIIRO <miirosadat0@gmail.com>
2026-08-06 10:14:12 +0300

    Add cart contents

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,13 +1,59 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import emptyCart from "../assets/images/emptycart.png";
+import { FaTrashAlt } from "react-icons/fa";
 
 const Cart = () => {
   const cart = useSelector((state) => state.cart);
   return (
     <div>
       {cart.products.length > 0 ? (
-        <div></div>
+        <div>
+          <h3>SHOPPING CART</h3>
+          <div>
+            <div>
+              <div>
+                <p>PRODUCTS</p>
+                <div>
+                  <p>PRICE</p>
+                  <p>QUANTITY</p>
+                  <p>SUBTOTAL</p>
+                  <p>REMOVE</p>
+                </div>
+              </div>
+              <div>
+                {cart.products.map((product) => (
+                  <div>
+                    <div>
+                      <img src={product.image} />
+                      <div>
+                        <h3>{product.name}</h3>
+                        <div className="flex">
+                          <button>-</button>
+                          <p>{product.quantity}</p>
+                          <button>+</button>
+                        </div>
+
+                        <p>
+                          UGX {(product.quantity * product.price).toFixed(2)}
+                        </p>
+                        <button>
+                          <FaTrashAlt />
+                        </button>
+                      </div>
+                    </div>
+
+                    <div>
+                      <p>UGX {product.price}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            <div></div>
+          </div>
+        </div>
       ) : (
         <div className="flex justify-center">
           <img src={emptyCart} className="h-96" />
