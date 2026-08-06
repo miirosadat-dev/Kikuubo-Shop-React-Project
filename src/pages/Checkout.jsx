6e9a871ed4d5194438c5c4f678e69fde2afce0cb
SADAT MIIRO <miirosadat0@gmail.com>
2026-08-06 22:42:40 +0300

    Add changes in checkout component

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -59,7 +59,7 @@ const Checkout = () => {
     }
   };
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-49">
       <div className="container mx-auto px-4 py-10 lg:px-16 xl:px-24">
         {/* Header */}
         <div className="mb-10">
